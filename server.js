aa6f5cb158986221b2d45d8b4b3776741f8dea1d
Roy Mollenkamp <rmollenkamp@gmail.com>
2020-11-25 22:12:59 -0700

    Updated API with Mongo

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 ﻿require('rootpath')();
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('_middleware/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cookieParser());
 
-// use JWT auth to secure the api
-app.use(jwt());
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
 // api routes
-app.use('/users', require('./users/users.controller'));
+app.use('/accounts', require('./accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('_helpers/swagger'));
 
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
